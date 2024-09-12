f9dd7d5d8f79fe1ef7d55e020ac0b819b977a523
Srishti Agarwal <agarwal11srishti@gmail.com>
2024-09-12 13:26:05 +0530

    Modified: db.js

--- backend/db.js
+++ backend/db.js
@@ -4,12 +4,12 @@ import 'dotenv/config';
 let client = null;
 
 async function connectToMongo() {
-    if (!client) {
-      client = new MongoClient(process.env.MONGODB_URI);
-      await client.connect();
-      console.log('MongoDB connected successfully');
+    if (!client || !client.topology || !client.topology.isConnected()) {
+        client = new MongoClient(process.env.MONGODB_URI);
+        await client.connect();
+        console.log('MongoDB connected successfully');
     }
     return client;
-  }  
+}
 
 export { connectToMongo };
