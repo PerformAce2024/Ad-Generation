fabecda576d2a00d9a7d4ad60e7c21015e1fc214
Srishti Agarwal <agarwal11srishti@gmail.com>
2024-09-18 17:10:43 +0530

    modified:   Frontend/ads.js

--- Frontend/ads.js
+++ Frontend/ads.js
@@ -39,7 +39,6 @@ async function onGetCreativesHandler(event) {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            email: email,
             google_play: googlePlayURL,
             apple_app: appleAppURL,
         })
